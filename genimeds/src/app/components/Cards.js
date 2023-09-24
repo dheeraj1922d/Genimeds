a450b6c590d9b560bc24aac79982ba353b1dd963
Dhiraj <rd6999787@gmail.com>
2023-09-24 16:15:36 +0530

    frontend has been completed in newItems and api fetch will be done sortly

---
+++ genimeds/src/app/components/Cards.js
@@ -0,0 +1,100 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+        
+        <div className="flex h-[1064px] items-center gap-[20px] px-[75px] py-[25px] relative">
+      <div className="flex-col w-[496px] items-center px-[20px] py-[10px] bg-white shadow-[0px_2px_25px_#0000001f] flex gap-[20px] relative self-stretch">
+        <img className="relative w-[250px] h-[250px] object-cover" alt="Download" src="download-1.png" />
+        <p className="relative self-stretch [font-family:'Inter-Bold',Helvetica] font-bold text-black text-[32px] tracking-[0] leading-[normal]">
+          Vicks® VapoRub™ Topical Cough Suppressant
+        </p>
+        <div className="items-start px-0 py-[2px] w-full flex-[0_0_auto] flex gap-[20px] relative self-stretch">
+          <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-black text-[32px] tracking-[0] leading-[normal]">
+            MRP
+          </div>
+          <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Bold',Helvetica] font-bold text-red-500 text-[32px] tracking-[0] leading-[normal]">
+            79.00 INR
+          </div>
+        </div>
+        <p className="relative self-stretch [font-family:'Inter-Bold',Helvetica] font-normal text-transparent text-[20px] tracking-[0] leading-[30px]">
+          <span className="font-bold text-black">
+            PRODUCT DETAILS
+            <br />
+          </span>
+          <span className="[font-family:'Inter-Light',Helvetica] font-light text-zinc-500">
+            <br />
+            On chest and throat, temporarily relieves cough due to minor throat and bronchial irritation associated with
+            the common cold
+            <br />
+            On muscles and joints, temporarily relieves minor aches and pains of muscles and joints
+          </span>
+        </p>
+        <p className="relative self-stretch [font-family:'Inter-Bold',Helvetica] font-normal text-transparent text-[20px] tracking-[0] leading-[30px]">
+          <span className="font-bold text-black">
+            ACTIVE INGREDIENTS
+            <br />
+          </span>
+          <span className="[font-family:'Inter-Light',Helvetica] font-light text-zinc-500">
+            <br />
+            Regular: Camphor (synthetic) 4.8% (Cough suppressant and topical analgesic) Eucalyptus oil 1.2% (Cough
+            suppressant) Menthol 2.6% (Cough suppressant and topical analgesic)
+            <br />
+            Lemon: Camphor (synthetic) 4.7% (Cough suppressant and topical analgesic) Eucalyptus oil 1.2% (Cough
+            suppressant) Menthol 2.6% (Cough suppressant and topical analgesic).
+          </span>
+        </p>
+      </div>
+      <div className="flex-col items-center px-[40px] py-[10px] flex-1 grow bg-white shadow-[0px_4px_25px_#00000021] flex gap-[20px] relative self-stretch">
+        <img
+          className="relative w-[375px] h-[250px] object-cover"
+          alt="Menthol terpineol"
+          src="menthol-terpineol-oil-chlorothymol-camphor-eucalyptus-oil-soft-gel-capsules-1.png"
+        />
+        <p className="relative self-stretch [font-family:'Inter-Bold',Helvetica] font-bold text-black text-[32px] tracking-[0] leading-[normal]">
+          Camphor, Menthol, and Eucalyptus Oil Topical Ointment
+        </p>
+        <div className="items-start px-0 py-[2px] w-full flex-[0_0_auto] flex gap-[20px] relative self-stretch">
+          <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-black text-[32px] tracking-[0] leading-[normal]">
+            MRP
+          </div>
+          <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Bold',Helvetica] font-bold text-lime-500 text-[32px] tracking-[0] leading-[normal]">
+            19.00 INR
+          </div>
+        </div>
+        <p className="relative self-stretch [font-family:'Inter-Bold',Helvetica] font-normal text-transparent text-[20px] tracking-[0] leading-[30px]">
+          <span className="font-bold text-black">
+            ACTIVE INGREDIENTS
+            <br />
+          </span>
+          <span className="[font-family:'Inter-Light',Helvetica] font-light text-zinc-500">
+            <br />
+            Regular: Camphor (synthetic) 4.8% (Cough suppressant and topical analgesic) Eucalyptus oil 1.2% (Cough
+            suppressant) Menthol 2.6% (Cough suppressant and topical analgesic)
+            <br />
+            Lemon: Camphor (synthetic) 4.7% (Cough suppressant and topical analgesic) Eucalyptus oil 1.2% (Cough
+            suppressant) Menthol 2.6% (Cough suppressant and topical analgesic).
+          </span>
+        </p>
+        <p className="relative self-stretch [font-family:'Inter-Bold',Helvetica] font-normal text-transparent text-[20px] tracking-[0] leading-[30px]">
+          <span className="font-bold text-black">
+            ACTIVE INGREDIENTS
+            <br />
+          </span>
+          <span className="[font-family:'Inter-Light',Helvetica] font-light text-zinc-500">
+            <br />
+            Regular: Camphor (synthetic) 4.8% (Cough suppressant and topical analgesic) Eucalyptus oil 1.2% (Cough
+            suppressant) Menthol 2.6% (Cough suppressant and topical analgesic)
+            <br />
+            Lemon: Camphor (synthetic) 4.7% (Cough suppressant and topical analgesic) Eucalyptus oil 1.2% (Cough
+            suppressant) Menthol 2.6% (Cough suppressant and topical analgesic).
+          </span>
+        </p>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Cards
---
+++ genimeds/src/app/components/SearchBtn.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SearchBtn = () => {
+  return (
+    
+    <div className="flex h-[40px] w-[500px] items-center gap-[10px] p-[10px] pr-0 relative bg-[#00000005] rounded-[8px] overflow-hidden border border-solid border-[#00000040] ml-[100px]">
+      <img className="w-[18px] h-[18px]" alt="Vector" src="vector.svg" />
+     
+      <input type='text' placeholder="Browse Our Catalog." className="w-[500px]  border-none outline-none mt-[-0.50px] [font-family:'Inter-Light',Helvetica]  text-[#00000040] text-[16px] tracking-[0] leading-[normal] whitespace-nowrap"/>
+
+      <buttion className="cursor-pointer"
+      ><div className="right-11 flex  justify-center items-center w-[117px] h-[40px] bg-white rounded-[8px] [background:linear-gradient(180deg,rgb(101,163,13)_0%,rgb(163,230,53)_100%)]">search</div></buttion>
+      
+      
+    
+    </div>
+    
+  )
+}
+
+export default SearchBtn
